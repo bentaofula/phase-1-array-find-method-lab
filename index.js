3ce3edf71c90427281fccdb25800d064ead18861
Benta <bentaakitwi500@gmail.com>
2023-03-20 00:04:40 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(array1){ 
+    let winning =  array1.find((winner) => winner["result"] === 'W')
+    if (winning)
+    {
+        return winning.year
+    }
+    else {
+        return undefined;
+    }}
